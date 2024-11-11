8237742ebc99534774922b5979918d5bfde96f1b
Brent Mills <brent.c.mills@gmail.com>
2024-11-11 10:21:43 -0500

    reworking users/services/mail.py to use the mailtrap Mail object construct.

--- users/services/mail.py
+++ users/services/mail.py
@@ -1,5 +1,5 @@
 import logging
-from mailtrap import MailtrapClient
+import mailtrap as mt
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
@@ -8,11 +8,11 @@ logger = logging.getLogger(__name__)
 
 class EmailService:
     def __init__(self):
-        self.client = MailtrapClient(token=settings.MAILTRAP_API_TOKEN)
-        self.sender = {
-            "email": settings.MAILTRAP_SENDER_EMAIL,
-            "name": "MyBlueList"
-        }
+        self.client = mt.MailtrapClient(token=settings.MAILTRAP_API_TOKEN)
+        self.sender = mt.Address(
+            email=settings.MAILTRAP_SENDER_EMAIL,
+            name="MyBlueList"
+        )
 
     def send_email(self, to_email, subject, html_content, text_content=None):
         """
@@ -22,16 +22,18 @@ class EmailService:
             if text_content is None:
                 text_content = strip_tags(html_content)
 
-            response = self.client.send(
+            mail = mt.Mail(
                 sender=self.sender,
-                to=[{"email": to_email}],
+                to=[mt.Address(email=to_email)],
                 subject=subject,
-                html_body=html_content,
-                text_body=text_content
+                html=html_content,
+                text=text_content
             )
+
+            response = self.client.send(mail)
             
-            logger.info(f"Email sent successfully to {to_email}. Message ID: {response.message_id}")
-            return True, response.message_id
+            logger.info(f"Email sent successfully to {to_email}.")
+            return True, None
 
         except Exception as e:
             logger.error(f"Failed to send email to {to_email}: {str(e)}", exc_info=True)
